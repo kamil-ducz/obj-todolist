e462050f8f57bfbbf696307c0a12d4990f63002b
Kamil D <kamilducz1989@gmail.com>
2022-07-29 21:38:03 +0200

    Task 2-4 basic solution

---
+++ ToDoList/ToDoList.Domain/Models/BucketTask.cs
@@ -0,0 +1,27 @@
+﻿namespace ToDoList.Domain.Models
+{
+    public class BucketTask
+    {
+        public int Id { get; set; }
+        public int BucketId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public TaskState TaskState { get; set; }
+        public TaskPriority TaskPriority { get; set; }
+    }
+
+    public enum TaskState
+    {
+        ToDo,
+        InProgress,
+        Done,
+        Cancelled
+    }
+
+    public enum TaskPriority
+    {
+        High,
+        Normal,
+        Low
+    }
+}
--- ToDoList/ToDoList.Infrastructure/Database.cs
+++ ToDoList/ToDoList.Infrastructure/Database.cs
@@ -1,6 +1,12 @@
-﻿namespace ToDoList.Infrastructure
+﻿using ToDoList.Domain.Models;
+
+namespace ToDoList.Infrastructure
 {
     public static class Database
     {
+        public static Bucket? Bucket { get; set; }
+        public static BucketTask? BucketTask { get; set; }
+        public static Assignee? Assignee { get; set; }
+        public static Stats? Stats { get; set; }
     }
 }
