c11610ba0833c101f31c3b5bd3a6e3651a4be84c
Kamil D <kamilducz1989@gmail.com>
2022-08-01 21:40:11 +0200

    Tasks4-5

---
+++ ToDoList/ToDoList.Api/Interfaces/IBucketTask.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ToDoList.Domain.Models;
+
+namespace ToDoList.Domain.Interfaces
+{
+    public interface IBucketTask
+    {
+        List<BucketTask> GetAllTasks();
+        BucketTask GetTask(int taskId);
+        int InsertBucketTask(int taskId);
+        void DeleteBucketTask(int taskId);
+        void UpdateBucketTask(int taskId);
+    }
+}
