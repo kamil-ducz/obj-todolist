81a3f9d51cea201556cc3e6bf8a8ac2a8042b2fb
Kamil D <kamilducz1989@gmail.com>
2022-08-11 12:03:40 +0200

    DbContext added, ConnectionString added in appsettings.json, migration populate local db with 4 tables

---
+++ ToDoList/ToDoList.Api/ToDoListDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ToDoList.Api
+{
+    public class ToDoListDbContext : DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public ToDoListDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Domain.Models.Assignee>()
+        //                .Property(n => n.Name)
+        //                .IsRequired();
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(configuration.GetConnectionString("ToDoListDatabase"));
+        }
+
+        public DbSet<Domain.Models.Assignee>? Assignees { get; set; }
+        public DbSet<Domain.Models.Bucket>? Buckets { get; set; }
+        public DbSet<Domain.Models.BucketTask>? BucketTasks { get; set; }
+        public DbSet<Domain.Models.Stats>? Stats { get; set; }
+    }
+}
