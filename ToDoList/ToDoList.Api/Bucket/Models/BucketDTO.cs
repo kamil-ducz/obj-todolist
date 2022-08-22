0c539f23e1257836c3ecd6a402f4931488d074e8
Kamil D <kamilducz1989@gmail.com>
2022-08-22 17:44:57 +0200

    Error fixed, domain model was field instead dto model for Bucket entity

--- ToDoList/ToDoList.Api/Bucket/Models/BucketDTO.cs
+++ ToDoList/ToDoList.Api/Bucket/Models/BucketDTO.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using ToDoList.Api.Asignee.Models;
 using ToDoList.Api.BucketTask.Models;
 using static ToDoList.Domain.Enums.Enums;
 
@@ -11,7 +12,7 @@ namespace ToDoList.Api.Bucket.Models
         public string? Description { get; set; }
         public string? CategoryName { get; set; }
         public int AssigneeId { get; set; }
-        public virtual Domain.Models.Assignee Assignee { get; set; }
+        public virtual AssigneeDTO? Assignee { get; set; }
         public Category Category { get; set; }
         public BucketColor BucketColor { get; set; }
         public List<BucketTaskDTO>? Tasks { get; set; }
