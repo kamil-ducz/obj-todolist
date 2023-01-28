4e4f8a63f6e416b5635f885941554023e07b3944
Kamil D <kamilducz1989@gmail.com>
2023-01-28 10:12:08 +0100

    Add bucket component

--- ToDoList-frontend/src/app/app.module.ts
+++ ToDoList-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { BucketComponent } from './bucket/bucket.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    StatisticsComponent
+    StatisticsComponent,
+    BucketComponent
   ],
   imports: [
     BrowserModule,
