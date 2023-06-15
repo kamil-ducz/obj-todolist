92db2f4192909561e7bd8b32f1a2d0ccc0bc9b15
Kamil D <kamilducz1989@gmail.com>
2023-06-15 15:51:28 +0200

    register and add assignee service skeleton

--- ToDoList-frontend/src/app/services/assignee-service.ts
+++ ToDoList-frontend/src/app/services/assignee-service.ts
@@ -1,4 +1,27 @@
-// backend controller file: AssigneeController.cs
+import { HttpClient } from "@angular/common/http";
+
 export class AssigneeService {
 
+    constructor(private httpClient: HttpClient) {}
+
+    getAssignees(): void {
+        this.httpClient.get('https://localhost:7247/api/Assignee');
+    }
+
+    getAssignee(id:number): void {
+        this.httpClient.get('https://localhost:7247/api/Assignee/{id}');
+    }
+
+    postAssignee(id:number): void {
+        this.httpClient.post('https://localhost:7247/api/Assignee', null); //2nd argument to work out TODO
+    }
+
+    putAssignee(id:number): void {
+        this.httpClient.put('https://localhost:7247/api/Assignee', null);
+    }
+    
+    deleteAssignee(id:number): void {
+        this.httpClient.delete('https://localhost:7247/api/Assignee');
+    }
+    
 }
