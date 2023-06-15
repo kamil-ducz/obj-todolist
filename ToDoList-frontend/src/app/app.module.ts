64b85d18b292937752c5cec3c0ca9d315c901101
Kamil D <kamilducz1989@gmail.com>
2023-06-15 12:16:36 +0200

    register and add statistics service skeleton

--- ToDoList-frontend/src/app/app.module.ts
+++ ToDoList-frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { BucketComponent } from './bucket/bucket.component';
 import { BucketsComponent } from './buckets/buckets.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { BucketsComponent } from './buckets/buckets.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ToDoList-frontend/src/app/services/stats-service.ts
+++ ToDoList-frontend/src/app/services/stats-service.ts
@@ -1,4 +1,26 @@
-// backend controller file: StatsController.cs
-export class StatsService {
+import { HttpClient } from '@angular/common/http';
 
+export class StatsService {
+    
+    constructor(private httpClient: HttpClient) {}
+
+    getStats(): void {
+        this.httpClient.get('https://localhost:7247/api/Stats');
+    }
+
+    getStat(id:number): void {
+        this.httpClient.get('https://localhost:7247/api/Stats/{id}');
+    }
+
+    postStat(id:number): void {
+        this.httpClient.post('https://localhost:7247/api/Stats', null); //2nd argument to work out TODO
+    }
+
+    putStat(id:number): void {
+        this.httpClient.put('https://localhost:7247/api/Stats', null);
+    }
+
+    deleteStat(id:number): void {
+        this.httpClient.delete('https://localhost:7247/api/Stats');
+    }
 }
