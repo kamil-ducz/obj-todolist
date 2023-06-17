18a2f8f0f59f007a5c54688b57cccff2f3afce11
Kamil D <kamilducz1989@gmail.com>
2023-06-17 13:21:20 +0200

    Fix frontend stat namings

--- ToDoList-frontend/src/app/statistics/statistics.component.ts
+++ ToDoList-frontend/src/app/statistics/statistics.component.ts
@@ -11,10 +11,10 @@ export class StatisticsComponent implements OnInit {
   constructor(private statsService:StatsService) { }
 
   statData: any;
-  percentOfTasksToDo: number = 0;
-  percentOfTasksInProgress: number = 0;
-  percentOfTasksCompleted: number = 0;
-  percentOfTasksCancelled: number = 0;
+  ToDo: number = 0;
+  InProgress: number = 0;
+  Completed: number = 0;
+  Cancelled: number = 0;
 
   ngOnInit(): void {
     // getStat(id) where id should be currently logged user AssigneId TODO
@@ -22,10 +22,10 @@ export class StatisticsComponent implements OnInit {
     this.statsService.getStat(8).subscribe(
       (response: any) => {
         this.statData = response;
-        this.percentOfTasksToDo = response.percentOfTasksToDo;
-        this.percentOfTasksInProgress = response.percentOfTasksInProgress;
-        this.percentOfTasksCompleted = response.percentOfTasksCompleted;
-        this.percentOfTasksCancelled = response.percentOfTasksCancelled;
+        this.ToDo = response.toDo;
+        this.InProgress = response.inProgress;
+        this.Completed = response.completed;
+        this.Cancelled = response.cancelled;
       },
       (error: any) => {
         console.error(error);        
