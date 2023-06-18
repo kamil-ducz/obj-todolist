2e5382627da942371a1ebfacd5a1892e7198e52a
Kamil D <kamilducz1989@gmail.com>
2023-06-18 07:07:32 +0200

    Update buckets component, add sample routing

--- ToDoList-frontend/src/app/app-routing.module.ts
+++ ToDoList-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BucketComponent } from './bucket/bucket.component';
+import { BucketsComponent } from './buckets/buckets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BucketsComponent },
+  { path: 'bucket', component: BucketComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
