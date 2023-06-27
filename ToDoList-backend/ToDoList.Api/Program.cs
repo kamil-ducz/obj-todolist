40e3eccae6ab43b9b99cc714e31c93e78573111e
Kamil D <kamilducz1989@gmail.com>
2023-06-27 12:18:50 +0200

    Extract services injection to separate file

--- ToDoList-backend/ToDoList.Api/Program.cs
+++ ToDoList-backend/ToDoList.Api/Program.cs
@@ -1,18 +1,6 @@
-using FluentValidation;
-using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using ToDoList.Api.Asignee.Models;
-using ToDoList.Api.Assignee.Services;
-using ToDoList.Api.Bucket.Models;
-using ToDoList.Api.Bucket.Services;
-using ToDoList.Api.BucketTask.Models;
-using ToDoList.Api.BucketTask.Services;
-using ToDoList.Api.Validation;
-using ToDoList.Domain.Interfaces;
-using ToDoList.Infrastructure.Repositories;
+using ToDoList.Api.Config;
 
 namespace ToDoList.Api;
 
@@ -23,34 +11,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddControllers();
-        builder.Services.AddCors();
-        builder.Services.AddMvc();
-        builder.Services.AddFluentValidationAutoValidation();
-
-        builder.Services.AddScoped<IAssigneeService, AssigneeService>();
-        builder.Services.AddScoped<IBucketService, BucketService>();
-        builder.Services.AddScoped<IBucketTaskService, BucketTaskService>();
-
-        builder.Services.AddScoped<IAssigneeRepository, AssigneeRepository>();
-        builder.Services.AddScoped<IBucketRepository, BucketRepository>();
-        builder.Services.AddScoped<IBucketTaskRepository, BucketTaskRepository>();
-
-        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-        builder.Services.AddScoped<IValidator<AssigneeDto>, AssigneeDtoValidator>();
-        builder.Services.AddScoped<IValidator<AssigneeInsertDto>, AssigneeInsertDtoValidator>();
-        builder.Services.AddScoped<IValidator<BucketDto>, BucketDtoValidator>();
-        builder.Services.AddScoped<IValidator<BucketInsertDto>, BucketInsertDtoValidator>();
-        builder.Services.AddScoped<IValidator<BucketTaskDto>, BucketTaskDtoValidator>();
-        builder.Services.AddScoped<IValidator<BucketInsertTaskDto>, BucketInsertTaskDtoValidator>();
-
-        builder.Services.AddDbContext<ToDoListDbContext>();
+        DependencyInjection.AddApplicationServices(builder.Services);
 
         var app = builder.Build();
 
