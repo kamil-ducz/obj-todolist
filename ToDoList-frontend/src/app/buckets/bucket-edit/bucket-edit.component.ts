93c840ec60b2b8875666590416a34a7008667687
Kamil D <kamilducz1989@gmail.com>
2023-06-30 21:03:35 +0200

    Fix edit form fill bucket infos

--- ToDoList-frontend/src/app/buckets/bucket-edit/bucket-edit.component.ts
+++ ToDoList-frontend/src/app/buckets/bucket-edit/bucket-edit.component.ts
@@ -3,7 +3,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Bucket } from 'src/app/models/bucket.model';
+import { BucketCategory } from 'src/app/models/bucketCategory.model';
+import { BucketColor } from 'src/app/models/bucketColor.model';
 import { BucketService } from 'src/app/services/bucket-service';
+import { DictionaryService } from 'src/app/services/dictionary.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -12,29 +15,53 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./bucket-edit.component.css']
 })
 export class BucketEditComponent implements OnInit {
-
   id: number;
   currentBucket: Bucket;
   currentBucketColorName: string;
   currentBucketCategoryName: string;
   editBucketFormGroup: FormGroup;
 
-  constructor(private route: ActivatedRoute, private bucketService: BucketService, private toastr: ToastrService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private bucketService: BucketService,
+    private dictionaryService: DictionaryService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.id = +params['id'];
-      }
-    )
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
 
-    this.bucketService.getBucket(environment.bucketEndpoint+this.id).subscribe(
+      this.bucketService.getBucket(environment.bucketEndpoint + this.id).subscribe(
+        (response: any) => {
+          this.currentBucket = response;
+          this.initializeForm();
+        },
+        (error: any) => {
+          this.toastr.error("Request failed. Check console logs and network tab to identify the issue." + error.name);
+        }
+      );
+    });
+  }
+
+  convertBucketColorIdToName(bucketColorId: number): void {
+    this.dictionaryService.getBucketColorNameById(environment.bucketColorEndpoint + bucketColorId).subscribe(
       (response: any) => {
-        this.currentBucket = response;        
-        this.initializeForm();
+        this.currentBucketColorName = response.name;
       },
       (error: any) => {
-        this.toastr.error("Request failed. Check console logs and network tab to identify the issue.")
+        this.toastr.error("Request failed. Check console logs and network tab to identify the issue.");
+      }
+    );
+  }
+
+  convertBucketCategoryIdToName(bucketCategoryId: number): void {
+    this.dictionaryService.getBucketCategoryNameById(environment.bucketCategoryEndpoint + bucketCategoryId).subscribe(
+      (response: any) => {
+        this.currentBucketCategoryName = response.name;
+      },
+      (error: any) => {
+        this.toastr.error("Request failed. Check console logs and network tab to identify the issue.");
       }
     );
   }
@@ -48,10 +75,10 @@ export class BucketEditComponent implements OnInit {
       description: new FormControl(this.currentBucket.description, [
         Validators.maxLength(50),
       ]),
-      bucketCategory: new FormControl(this.currentBucket.bucketCategoryId, [
+      bucketCategory: new FormControl('', [
         Validators.required,      
       ]),
-      bucketColor: new FormControl(this.currentBucket.bucketColorId, [
+      bucketColor: new FormControl('', [
         Validators.required,
       ]),
       maxAmountOfTasks: new FormControl(this.currentBucket.maxAmountOfTasks.toString(), [
@@ -63,11 +90,38 @@ export class BucketEditComponent implements OnInit {
         Validators.required,
       ])
     });
+
+    this.loadBucketCategoryName(this.currentBucket.bucketCategoryId);
+    this.loadBucketColorName(this.currentBucket.bucketColorId);
+  }
+
+  loadBucketCategoryName(bucketCategoryId: number) {
+    this.dictionaryService.getBucketCategoryNameById(environment.bucketCategoryEndpoint + bucketCategoryId)
+      .subscribe(
+        (response: BucketCategory) => {
+          this.editBucketFormGroup.patchValue({ bucketCategory: response.name });
+        },
+        (error: any) => {
+          this.toastr.error("Failed to retrieve bucket category name. Check console logs and network tab to identify the issue.");
+        }
+      );
+  }
+  
+  loadBucketColorName(bucketColorId: number) {
+    this.dictionaryService.getBucketColorNameById(environment.bucketColorEndpoint + bucketColorId)
+      .subscribe(
+        (response: BucketColor) => {
+          this.editBucketFormGroup.patchValue({ bucketColor: response.name });
+        },
+        (error: any) => {
+          this.toastr.error("Failed to retrieve bucket color name. Check console logs and network tab to identify the issue.");
+        }
+      );
   }
 
   onSubmitEditForm(bucketFromEditForm: Bucket) {
     this.currentBucket = bucketFromEditForm;
-    this.bucketService.putBucket(environment.bucketEndpoint+this.id, this.currentBucket).subscribe(
+    this.bucketService.putBucket(environment.bucketEndpoint + this.id, this.currentBucket).subscribe(
       (response: any) => {
         console.log(response);
       },
