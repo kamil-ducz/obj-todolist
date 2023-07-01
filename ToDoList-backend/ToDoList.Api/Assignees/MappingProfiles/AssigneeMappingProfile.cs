ca0e04760966506eb124be7f2c06bf42bd6642f7
Rafal Kobza <rkobza@objectivity.co.uk>
2023-07-01 14:29:44 +0200

    Cleanup mapping profiles

---
+++ ToDoList-backend/ToDoList.Api/Assignees/MappingProfiles/AssigneeMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ToDoList.Api.Assignees.Models;
+using ToDoList.Domain.Models;
+
+namespace ToDoList.Api.Assignees.MappingProfiles;
+
+public class AssigneeMappingProfile : Profile
+{
+    public AssigneeMappingProfile()
+    {
+        CreateMap<Assignee, AssigneeDto>().ReverseMap();
+        CreateMap<Assignee, AssigneeUpsertDto>().ReverseMap();
+    }
+}
