e09105a021078d55de64fdb28876f09a8b23291b
Kamil D <kamilducz1989@gmail.com>
2023-07-11 08:30:58 +0200

    Add more example assignees to seed

---
+++ ToDoList-backend/ToDoList.Infrastructure/Migrations/20230711062936_Add more assignees to seed.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Infrastructure.Migrations
+{
+    public partial class Addmoreassigneestoseed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Assignees",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 2, "Otobong Shay" },
+                    { 3, "Pilirani Tendai" },
+                    { 4, "Jess Blue" },
+                    { 5, "Beck Itumeleng" },
+                    { 6, "Radha Mpho" },
+                    { 7, "Chikelu Tshepo" },
+                    { 8, "Amal Yu" },
+                    { 9, "Sasha Tristin" },
+                    { 10, "Lee Lihuén" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Assignees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Assignees",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Assignees",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Assignees",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Assignees",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Assignees",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Assignees",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Assignees",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Assignees",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
