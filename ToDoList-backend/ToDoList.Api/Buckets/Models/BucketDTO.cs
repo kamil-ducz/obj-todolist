fe4965c9652eaf737bf88817ba63c36a099468aa
Kamil D <kamilducz1989@gmail.com>
2023-07-13 08:41:04 +0200

    Code review: fix bucket dtos

--- ToDoList-backend/ToDoList.Api/Buckets/Models/BucketDTO.cs
+++ ToDoList-backend/ToDoList.Api/Buckets/Models/BucketDTO.cs
@@ -1,19 +1,3 @@
-﻿using System.Collections.Generic;
-using ToDoList.Api.BucketTasks.Models;
-using ToDoList.Domain.Models;
+﻿namespace ToDoList.Api.Buckets.Models;
 
-namespace ToDoList.Api.Buckets.Models;
-
-public class BucketDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string? Description { get; set; }
-    public int BucketCategoryId { get; set; }
-    public BucketCategory? BucketCategory { get; set; }
-    public int BucketColorId { get; set; }
-    public BucketColor? BucketColor { get; set; }
-    public int MaxAmountOfTasks { get; set; } = 1;
-    public bool IsActive { get; set; }
-    public List<BucketTaskDto> BucketTask { get; set; } = new List<BucketTaskDto>();
-}
+public record BucketDto(int Id, string Name, string? Description, int BucketCategoryId, int BucketColorId, int MaxAmountOfTasks, bool IsActive);
