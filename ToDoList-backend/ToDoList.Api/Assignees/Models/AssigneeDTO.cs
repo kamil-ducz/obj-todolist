14121ee1df6245f5ef8cd9ef3550dfb12ef4f75b
Kamil D <kamilducz1989@gmail.com>
2023-07-13 07:46:41 +0200

    Code review: fix assignee dtos

--- ToDoList-backend/ToDoList.Api/Assignees/Models/AssigneeDTO.cs
+++ ToDoList-backend/ToDoList.Api/Assignees/Models/AssigneeDTO.cs
@@ -1,11 +1,3 @@
-﻿using System.Collections.Generic;
-using ToDoList.Domain.Models;
+﻿namespace ToDoList.Api.Assignees.Models;
 
-namespace ToDoList.Api.Assignees.Models;
-
-public record AssigneeDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public List<BucketTask>? BucketTask { get; set; }
-}
+public record AssigneeDto(int Id, string Name);
--- ToDoList-backend/ToDoList.Api/Assignees/Models/AssigneeUpsertDto.cs
+++ ToDoList-backend/ToDoList.Api/Assignees/Models/AssigneeUpsertDto.cs
@@ -1,10 +1,3 @@
-﻿using System.Collections.Generic;
-using ToDoList.Domain.Models;
+﻿namespace ToDoList.Api.Assignees.Models;
 
-namespace ToDoList.Api.Assignees.Models;
-
-public class AssigneeUpsertDto
-{
-    public string Name { get; set; } = string.Empty;
-    public List<BucketTask>? BucketTask { get; set; }
-}
+public record AssigneeUpsertDto(string Name);
