1f8f8574b3383b971eafc68453403e48dc2afc48
Kamil D <kamilducz1989@gmail.com>
2023-08-01 09:47:06 +0200

    Add User entity to database with example explicit data

---
+++ ToDoList-backend/ToDoList.Infrastructure/DbContextConfigs/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoList.Domain.Models;
+
+namespace ToDoList.Infrastructure.DbContextConfigs;
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasData(
+            new User() { Id = 1, FirstName = "John", LastName = "Doe", Username = "JD", Password = "test" },
+            new User() { Id = 2, FirstName = "Ian", LastName = "Orange", Username = "IO", Password = "test2" },
+            new User() { Id = 3, FirstName = "Walter", LastName = "Silver", Username = "WS", Password = "test3" }
+            );
+    }
+}
