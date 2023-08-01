c08b4aac6fd4796628d61cce13a0f1b1508a9ff3
Kamil D <kamilducz1989@gmail.com>
2023-08-01 22:29:30 +0200

    Convert seeded passwords to hashes with salt

--- ToDoList-backend/ToDoList.Infrastructure/DbContextConfigs/UserConfiguration.cs
+++ ToDoList-backend/ToDoList.Infrastructure/DbContextConfigs/UserConfiguration.cs
@@ -8,9 +8,9 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.HasData(
-            new User() { Id = 1, FirstName = "John", LastName = "Doe", Username = "JD", Password = "test" },
-            new User() { Id = 2, FirstName = "Ian", LastName = "Orange", Username = "IO", Password = "test2" },
-            new User() { Id = 3, FirstName = "Walter", LastName = "Silver", Username = "WS", Password = "test3" }
+            new User() { Id = 1, FirstName = "John", LastName = "Doe", Username = "JD", Password = BCrypt.Net.BCrypt.HashPassword("test") },
+            new User() { Id = 2, FirstName = "Ian", LastName = "Orange", Username = "IO", Password = BCrypt.Net.BCrypt.HashPassword("test2") },
+            new User() { Id = 3, FirstName = "Walter", LastName = "Silver", Username = "WS", Password = BCrypt.Net.BCrypt.HashPassword("test3") }
             );
     }
 }
