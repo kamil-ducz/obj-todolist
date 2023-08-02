04d031fa1e900545c5006e21be0aed1796556144
Kamil D <kamilducz1989@gmail.com>
2023-08-02 09:46:27 +0200

    Add endpoint to add new user

--- ToDoList-backend/ToDoList.Api/Users/Controllers/UsersController.cs
+++ ToDoList-backend/ToDoList.Api/Users/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using ToDoList.Api.Users.Authorization;
 using ToDoList.Api.Users.Models;
 using ToDoList.Api.Users.Services;
+using ToDoList.Domain.Models;
 
 namespace ToDoList.Api.Users.Controllers;
 
@@ -34,5 +35,13 @@ public class UsersController : ControllerBase
     {
         var users = _userService.GetAll();
         return Ok(users);
+    }
+
+    [AllowAnonymous]
+    [HttpPost]
+    public IActionResult InsertNewUser(User user)
+    {
+        var userId = _userService.InsertNewUser(user);
+        return Ok(userId);
     }
 }
--- ToDoList-backend/ToDoList.Domain/Repositories/IUserRepository.cs
+++ ToDoList-backend/ToDoList.Domain/Repositories/IUserRepository.cs
@@ -6,8 +6,8 @@ namespace ToDoList.Domain.Repositories;
 public interface IUserRepository
 {
     IReadOnlyList<User> GetAllUsers();
-    User GetUser(int assigneeId);
-    void InsertUser(User assignee);
-    void DeleteUser(User assignee);
-    void UpdateUser(User assignee);
+    User GetUser(int userId);
+    void InsertUser(User user);
+    void DeleteUser(User user);
+    void UpdateUser(User user);
 }
